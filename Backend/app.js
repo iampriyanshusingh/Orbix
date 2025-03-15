70c433f65a834e0404f9193055dcec5976f28344
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-03-16 04:14:45 +0530

    `added db, models, routes, controllers`

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ dotenv.config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const connectToDb = require("./db/db");
+
+connectToDb();
 
 app.use(cors());
 
---
+++ Backend/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(process.env.DB_CONNECT)
+    .then(() => console.log("Connected to the db"))
+    .catch((err) => console.log(err));
+}
+
+module.exports = connectToDb;
