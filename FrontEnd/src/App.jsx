8ad2321d802cfbdc9b7d37a8a0644d0e57deb98f
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-04-08 21:39:50 +0530

    `siu`

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Body from "./Components/Body";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-      <Body/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 };
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className="text-2xl">
-      This is my home page.
-    </div>
-  )
-}
+    <>
+      <div>
+        <div className="h-screen flex justify-between flex-col w-full bg-red-400">
+          <img src="http://www.w3.org/2000/svg"></img>
+          <div className="bg-white">
+            <h2>Get Started with Orbix</h2>
+            <button>Continue</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
