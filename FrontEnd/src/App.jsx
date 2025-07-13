4580781d07449ac34891baf3cf4e9855b5d56c96
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-07-14 04:46:45 +0530

    `Added UserLogin page and updated App component to include new route`

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin/>}/>
+        <Route />
+        <Route />
+        <Route />
       </Routes>
     </>
   );
