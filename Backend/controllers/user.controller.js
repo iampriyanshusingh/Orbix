1877037a40b4bd79969bf1554e1775dfb78f741d
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-07-13 23:44:25 +0530

    `Added logout functionality and token blacklisting`

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
+// const blackListTokenModel = require("../models/blacklistedToken.model");
 
 //will take the response from the user by .post api
 module.exports.registerUser = async (req, res, next) => {
@@ -52,7 +53,9 @@ module.exports.loginUser = async (req, res, next) => {
 
   const token = user.generateAuthToken();
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+  });
 
   res.status(200).json({ token, user });
 };
@@ -63,4 +66,18 @@ module.exports.getUserProfile = async (req, res, next) => {
     return res.status(400).json({ message: errors.array() });
   }
   res.status(200).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+
+  res.cookie("token", token, {
+    expires: new Date(Date.now()),
+  });
+
+  // await blackListTokenModel.create({ token });
+
+  res.status(200).json({ message: "logged out successfully" });
 };
