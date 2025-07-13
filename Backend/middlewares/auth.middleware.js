e2ed6ab3ae478c6411742bc79e830909bf264652
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-07-13 23:55:39 +0530

    `Update auth middleware to handle null authorization headers and added Docs for frontend`

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
 module.exports.authUser = async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
