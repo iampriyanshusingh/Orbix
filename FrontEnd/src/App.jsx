ee9f8163fa2978e9b593c66efbe18d0847518eae
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-07-16 01:14:28 +0530

    `Added UserProtectedWrapper component and updated routes to protect user routes with authentication token`

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,6 +6,7 @@ import CaptainSignup from "./pages/CaptainSignup";
 import CaptainLogin from "./pages/CaptainLogin";
 import Start from "./pages/Start";
 import Home from "./pages/Home";
+import UserProtectedWrapper from "./pages/UserProtectedWrapper";
 
 const App = () => {
   return (
@@ -16,7 +17,14 @@ const App = () => {
         <Route path="/user-signup" element={<UserSignUp />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectedWrapper>
+              <Home />
+            </UserProtectedWrapper>
+          }
+        />
       </Routes>
     </>
   );
---
+++ FrontEnd/src/pages/UserProtectedWrapper.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { UserDataContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+const UserProtectedWrapper = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  if (!token) {
+    navigate("/login");
+  }
+  return <>{children}</>;
+};
+
+export default UserProtectedWrapper;
